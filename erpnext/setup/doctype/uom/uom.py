ff51ee028c01e80ad4a7014f0f05fd26c7feb967
RICKY SUTANTO <ricky.sutanto@gmail.com>
2025-07-01 23:13:10 +0700

    initial Commit

---
+++ erpnext/setup/doctype/uom/uom.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+
+from frappe.model.document import Document
+
+
+class UOM(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		enabled: DF.Check
+		must_be_whole_number: DF.Check
+		sync_id_api: DF.Int
+		uom_code: DF.Data | None
+		uom_name: DF.Data
+	# end: auto-generated types
+
+	pass
